e66c96cc930bafd74c27a161a81066c413d26066
abrocks1120gmail.com <abrocks1120gmail.com>
2024-01-18 20:36:19 +0530

    optimization

--- iconButton.js
+++ iconButton.js
@@ -6,7 +6,7 @@ customElements.define('ico-btn',
     connectedCallback() {
       const shadow = this.attachShadow({ mode: 'open' });
       const $ = document.createElement.bind(document);
-      
+
       const link = $('link');
       link.rel = 'stylesheet';
       link.href = 'iconButton.css';
@@ -14,12 +14,14 @@ customElements.define('ico-btn',
       const div = $('div');
 
       const button = $('button');
-      button.style.display='none';
-      
+      button.style.display = 'none';
+
       const img = $('img');
+      img.height = "100%";
+      img.width = "100%";
       img.addEventListener('load', () => {
         div.style.display = 'none';
-        button.style.display='block';
+        button.style.display = 'block';
       });
       img.src = this.dataset.icon;
 
@@ -31,10 +33,10 @@ customElements.define('ico-btn',
         if (!iconUrl) return;
         const name = prompt('Enter Button Name');
         if (!name) return;
-        const icoBtn = $('ico-btn');
-        icoBtn.dataset.icon = iconUrl;
-        icoBtn.textContent = name;
-        this.parentElement.appendChild(icoBtn);
+        button.style.display = 'none';
+        div.style.display = 'block';
+        img.src = iconUrl;
+        this.textContent = name;
       });
 
       shadow.append(link, div, button);
